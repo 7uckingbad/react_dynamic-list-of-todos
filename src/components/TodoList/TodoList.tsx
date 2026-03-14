eed9a40d41b6a4fbe083991b3936ba3094d83b6a
Denis M <denismushta@gmail.com>
2026-03-14 11:38:13 +0200

    sol2

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Todo } from '../../types/Todo';
+import cn from 'classnames';
 
 type Props = {
   todos: Todo[];
@@ -37,9 +38,10 @@ export const TodoList: React.FC<Props> = ({ todos, onShow, selectedTodo }) => (
           </td>
           <td className="is-vcentered is-expanded">
             <p
-              className={
-                todo.completed ? 'has-text-success' : 'has-text-danger'
-              }
+              className={cn({
+                'has-text-success': todo.completed,
+                'has-text-danger': !todo.completed,
+              })}
             >
               {todo.title}
             </p>
